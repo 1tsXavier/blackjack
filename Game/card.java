2ed55355c2c17de6bcd33868e8e7cb94c7a83294
1tsXavier <xavierreinhardt708@gmail.com>
2025-05-19 12:32:22 -0500

    Fleshed out Card Class and subclasses, working on Deck Class

--- Game/card.java
+++ Game/card.java
@@ -1,16 +1,39 @@
 public class Card
 {
-     
+    private int value;
+    private boolean isAce;
+    
+    public Card(int value, boolean isAce){
+        this.value = value;
+        this.isAce = isAce;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public boolean getAce(){
+        return isAce;
+    }
 }
 
-class Number extends Card {
+class Number extends Card{
 
+    public Number(int value){
+        super(value, false);
+    }
 }
 
 class Face extends Card {
 
+    public Face(){
+        super(10, false);
+    }
 }
 
 class Ace extends Card {
 
+    public Ace(){
+        super(1, true);
+    }
 }
