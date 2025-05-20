75e2c289db17ba2cde640cd758d70294dfeb5983
ShubhamS72 <solankishubham2009@gmail.com>
2025-05-20 12:19:24 -0500

    e

--- Game/deck.java
+++ Game/deck.java
@@ -2,4 +2,12 @@ public class Deck
 {
     private Card[][] deck;
     private ArrayList<Card> discard;
+
+    public Deck()
+    {
+        for(int i = 0;i<4;i++)
+        {
+            deck.add(new Ace();)
+        }
+    }
 }
