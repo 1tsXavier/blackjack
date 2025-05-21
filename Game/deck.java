9150b561048bbb8982e8858bb7195ac0fab9178c
ShubhamS72 <solankishubham2009@gmail.com>
2025-05-21 12:12:07 -0500

    deck class

--- Game/deck.java
+++ Game/deck.java
@@ -1,24 +1,31 @@
+
 public class Deck
 {
-    private Card[][] deck;
+    private ArrayList<Card> deck;
     private ArrayList<Card> discard;
 
     public Deck()
-    {
-        
+    {    
         for(int i = 0;i<4;i++)
         {
             deck.add(new Ace());
-            for(int i = 2;i<11;i++)
+            for(int k = 2;k<11;k++)
             {
-                deck.add(new Number(i));
+                deck.add(new Number(k));
             }
-            for(int i = 0;i<2;i++)
+            for(int j = 0;j<3;j++)
             {
                 deck.add(new Face());
             }
 
         }
-
     }
+
+    public card draw()
+    {
+        int rand = (int) Math.random(0,deck.size-1);
+    }
+
+    
+
 }
