fc738a3bb9f86c0da7735d0836f5f326f6156772
ShubhamS72 <solankishubham2009@gmail.com>
2025-05-21 11:55:02 -0500

    deck constructor

--- Game/deck.java
+++ Game/deck.java
@@ -5,9 +5,20 @@ public class Deck
 
     public Deck()
     {
+        
         for(int i = 0;i<4;i++)
         {
-            deck.add(new Ace();)
+            deck.add(new Ace());
+            for(int i = 2;i<11;i++)
+            {
+                deck.add(new Number(i));
+            }
+            for(int i = 0;i<2;i++)
+            {
+                deck.add(new Face());
+            }
+
         }
+
     }
 }
